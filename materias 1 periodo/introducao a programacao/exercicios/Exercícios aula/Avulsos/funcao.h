3efee74cd1c78f2e2df21e04705050fa9c536c30
marcusv77 <marcus.vsf@aluno.ufop.edu.br>
2021-02-12 22:25:05 -0300

    Update 12/02/2021

--- materias 1 periodo/introducao a programacao/exercicios/Exercícios aula/Avulsos/funcao.h
+++
@@ -1,3 +0,0 @@
-//Marcus Vinícius Souza Fernandes - 19.1.4046 - BCC201-61P
-
-void valorxy(int x,int y, float r);
--- materias 1 periodo/introducao a programacao/exercicios/Exercícios aula/Avulsos/loteria.h
+++
@@ -1,5 +0,0 @@
-//Marcus Vinícius Souza Fernandes - 19.1.4046 - BCC201-61P
-
-void tresvalores(int *n1, int *n2, int *n3);
-void sorteio(int *nmaleatorio1, int *nmaleatorio2, int *nmaleatorio3);
-double valorpremio(int premio, int *n1, int *n2, int *n3, int *nmaleatorio1, int *nmaleatorio2, int *nmaleatorio3);
